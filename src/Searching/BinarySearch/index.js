8b43ac89d53607d4500e1946e23511610e6420f9
Andrey Pavlov <beyondbirthday90@gmail.com>
2019-10-24 15:47:23 +0300

    Added iterative binarySearch algorithm

--- src/Searching/BinarySearch/index.js
+++ src/Searching/BinarySearch/index.js
@@ -14,4 +14,24 @@ const binarySearchRecursive = (arr, valueToFind, low = 0, high) => {
     return binarySearchRecursive(arr, valueToFind, middle + 1, high);
 };
 
+const iterativeBinarySearch = (arr, valueToFind) => {
+    let startIndex = 0;
+    let endIndex = arr.length - 1;
+
+    while (startIndex <= endIndex) {
+        const middleIndex = Math.floor((endIndex + startIndex) / 2);
+        const middleValue = arr[middleIndex];
+
+        if (middleValue === valueToFind) return middleIndex;
+        if (middleValue > valueToFind) {
+            endIndex = middleIndex - 1;
+        } else {
+            startIndex = middleIndex + 1;
+        }
+    }
+
+    return -1;
+};
+
 exports.binarySearchRecursive = binarySearchRecursive;
+exports.iterativeBinarySearch = iterativeBinarySearch;
